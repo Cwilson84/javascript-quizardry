e2fc8d08ea3dafd61d03922d1a0582035299ce7f
Nadiya Cruz-Toran <nadiyacruz-toran@Nadiyas-Air.lan>
2023-01-24 17:46:50 -0500

    question and answer object structure complete

--- script.js
+++ script.js
@@ -4,3 +4,65 @@ var optionOne = document.getElementById("#op1");
 var optionTwo = document.getElementById("#op2");
 var optionThree = document.getElementById("#op3");
 var optionFour = document.getElementById("#op4"); 
+
+var randomQuestions = [{
+    id: 0,
+    q: "?",
+    a: [{ text: "", isCorrect: false },
+        { text: "", isCorrect: false },
+        { text: "", isCorrect: false },
+        { text: "", isCorrect: true }
+    ]
+
+},
+{
+    id: 0,
+    q: "?",
+    a: [{ text: "", isCorrect: false },
+        { text: "", isCorrect: true },
+        { text: "", isCorrect: false },
+        { text: "", isCorrect: false }
+    ]
+
+},
+{
+    id: 0,
+    q: "?",
+    a: [{ text: "", isCorrect: false },
+        { text: "", isCorrect: false },
+        { text: "", isCorrect: true },
+        { text: "", isCorrect: false }
+    ]
+
+}, 
+{
+    id: 0,
+    q: "?",
+    a: [{ text: "", isCorrect: false },
+        { text: "", isCorrect: false },
+        { text: "", isCorrect: true },
+        { text: "", isCorrect: false }
+    ]
+
+}, 
+{
+    id: 0,
+    q: "?",
+    a: [{ text: "", isCorrect: true },
+        { text: "", isCorrect: false },
+        { text: "", isCorrect: false },
+        { text: "", isCorrect: false }
+    ]
+
+},
+{
+    id: 0,
+    q: "?",
+    a: [{ text: "", isCorrect: false },
+        { text: "", isCorrect: true},
+        { text: "", isCorrect: false },
+        { text: "", isCorrect: false }
+    ]
+
+}
+]
