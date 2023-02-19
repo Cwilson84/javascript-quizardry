a815136f0bd799b03da23972fb981d8de8fe700c
Chris Wilson <chrisw@myself.com>
2023-02-18 23:22:40 -0500

    revamp coming along nicely.

--- script.js
+++ script.js
@@ -12,43 +12,50 @@ var timerEl = document.getElementById("countdown");
 //Page greeting and name grab function
 function helloThere() {
     var greet = prompt("Please enter your name.");
-    var userName = greet ? greet.trim().toLowerCase() : "random internet user";
-    console.log(userName);
+    userName = greet ? greet.trim().charAt(0).toUpperCase() + greet.slice(1) : "random internet user";
+    console.log(`Hello, ${userName}. Enjoy the quiz!`);
 }
+//Grab name for console fun!
+helloThere();
 
 //Function to disable start button and begin timer when listener fires
 function startButtonClicked() {
     startButton.setAttribute("style", "background-color: darkgrey");
     startButton.style.pointerEvents = 'none';
     timerEl.innerHTML = timeLeft;
-    timer();
+    timerContainer.setAttribute("style", "background-color: green; width: 62vw");
+    setTimeout(timer, 500);
 };
 
 startButton.addEventListener("click", startButtonClicked);
 //Timer function complete with color change feature
 function timer() {
-    timerContainer.setAttribute("style", "background-color: green");
+    var shrinkingClock = 62;
+    timerContainer.setAttribute("style", "background-color: green; width: " + shrinkingClock + "vw");
     var intervalId = setInterval(() => {
-        console.log(timeLeft);
         if (timeLeft <= 31 && timeLeft >= 12) {
             timerContainer.setAttribute("style", "background-color: yellow");
-            console.log("Hurry up! Only " + timeLeft + " seconds left.");
+            console.log(`Hurry up, ${userName}! Only 30 seconds left.`);
         };
         if (timeLeft <= 11) {
             timerContainer.setAttribute("style", "background-color: red");
-            console.log("Less than 10 seconds left!");
+            console.log(`Less than 10 seconds left! You got this, ${userName}!`);
         };
         if (timeLeft === 1) {
             clearInterval(intervalId);
             timerContainer.setAttribute("style", "background-color: darkgrey");
-            console.log("Time's up. Do try again.");
+            console.log(`You're outta time, ${userName}. Refresh the page to try again, if you didn't bring a DMC DeLorean.`);
+            skipNextButton.setAttribute("style", "background-color: darkgrey");
+            skipNextButton.style.pointerEvents = 'none';
         };
         timeLeft--;
         timerEl.innerHTML = timeLeft;
+        shrinkingClock --;
+        timerContainer.style.width = shrinkingClock + "vw";
     }, 1000);
 };
 //Main content begins here
-helloThere();
+
 
 
 
